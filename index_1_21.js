2323e196e444ae20aa67f0b4ede04568f038d935
dmayers1204 <dmayers1204@gmail.com>
2024-01-22 00:10:16 -0500

    answer fourth function

--- index_1_21.js
+++ index_1_21.js
@@ -24,8 +24,9 @@ function getTarget(arr,target){
 
 // Given an array of numbers, return the first number that's bigger than 14 or undefined if no number is bigger than 14
 function greaterThan14(arr){
-    return arr.find((ele)=> ele > 14);  
+return arr.find((ele)=> ele > 14 ? ele : undefined) 
 }
+
 console.log(greaterThan14([3,4,5,6,7,17]));
 console.log(greaterThan14([3,4,5,19,7,8]));
 
@@ -45,18 +46,51 @@ console.log(returnFirstStr(['Yes','No','Bye','Hip','Bump','Hello']));
 
 // Given an array of Objects and a target id return the first object that matches the id or undefined if no such object is found
 // Expected Structure of a SINGLE object - 
-/*
-{
+
+const arrWitObjs = [{
  id: "Y42C_a",
  name: "Robert",
  occupation: "Chef",
  eyeColor: "green"
-}
-*/
+},
+{
+    id: "Y42C_b",
+    name: "Ralph",
+    occupation: "Chef",
+    eyeColor: "orange"
+   },
+   {
+    id: "Y42C_c",
+    name: "Ronny",
+    occupation: "Chef",
+    eyeColor: "yellow"
+   },
+   {
+    id: "Y42C_d",
+    name: "Robby",
+    occupation: "Chef",
+    eyeColor: "red"
+   },
+   {
+    id: "Y42C_e",
+    name: "Rick",
+    occupation: "Chef",
+    eyeColor: "blue"}];
 
+function returnFirstObj(arr,targetid){
+    for(let ele of arr) {
+    if(ele.id === targetid){
+    return ele;
+     }
+   }
+   return undefined; 
+} 
+console.log(returnFirstObj(arrWitObjs, "Y42C_d"));
+console.log(returnFirstObj(arrWitObjs, "Y42C_e"));
 
 // Given an array of objects and a target id return a new object with only the name. The structure for a single object in the array would be as the one above. The returned object would look like this - 
 /*
+
 {
  name: "Robert"
 }
